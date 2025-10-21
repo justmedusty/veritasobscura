823826560dfd95edfc6477ef78aa0203fe1f15a1
Dustyn <dustyn_gibb@hotmail.com>
2025-10-21 15:28:05 -0400

    file encoding trait

--- src/file_encoding_support/file_encoding_support.rs
+++ src/file_encoding_support/file_encoding_support.rs
@@ -16,53 +16,68 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  */
 
-    use std::fs::File;
-    use std::io::Error;
-pub struct ImageSupport<T : FileEncodingSupport> {
-    image_file : File,
-    encoding : FileEncoding,
-    encoding_method : FileEncodingMethod,
+use std::fs::File;
+use std::io::Error;
+pub struct ImageSupport<T: FileEncodingSupport + FileEncoding> {
+    image_file: File,
+    encoding: FileEncoding,
+    encoding_method: FileEncodingMethod,
     file_encoding_function_derivation: FileEncodingFunctionDerivation,
-    pixel_map : Vec<u8>,
-    encoding_support : T,
+    pixel_map: Vec<u8>,
+    encoding_support: T,
 }
-    #[derive(Copy, Clone, Debug, Eq, PartialEq)]
-    pub enum FileEncoding {
-        Lsb,
-        PixelValueDifferencing,
-        HammingMatrix,
-    }
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum FileEncoding {
+    Lsb,
+    PixelValueDifferencing,
+    HammingMatrix,
+}
 
-    #[derive(Copy, Clone, Debug, Eq, PartialEq)]
-    pub enum FileEncodingMethod {
-        LeftToRight,
-        TopToBottom,
-        SinWave,
-        CosWave,
-        PolynomialFunction,
-        FractalFunction,
-    }
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum FileEncodingMethod {
+    LeftToRight,
+    TopToBottom,
+    SinWave,
+    CosWave,
+    PolynomialFunction,
+    FractalFunction,
+}
 
-    #[derive(Copy, Clone, Debug, Eq, PartialEq)]
-    pub enum FileEncodingFunctionDerivation {
-        KeyBased,
-    }
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum FileEncodingFunctionDerivation {
+    KeyBased,
+}
 
-    pub trait FileEncodingSupport {
-        /*
-           Will return pixel map or will return error
-        */
-        fn parse_file(&mut self, file_location : &str);
+pub trait FileEncodingSupport {
+    /*
+       Will return pixel map or will return error
+    */
+    fn parse_file(&mut self, file_location: &str);
 
-        /*
-           Vec is JUST the pixel map not the entire file, needs to be pixel map since there may be decompression involved
-        */
-        fn embed_data(&mut self, data: &mut Vec<u8>);
+    /*
+       Vec is JUST the pixel map not the entire file, needs to be pixel map since there may be decompression involved
+    */
+    fn embed_data(&mut self, data: &mut Vec<u8>);
 
-        /*
-        Vec is JUST the pixel map not the entire file, needs to be pixel map since there may be decompression involved
-        */
-        fn retrieve_data(&mut self, data: &Vec<u8>);
+    /*
+    Vec is JUST the pixel map not the entire file, needs to be pixel map since there may be decompression involved
+    */
+    fn retrieve_data(&mut self, data: &Vec<u8>);
 
-        fn write_file(&mut self, file: &mut File, location: &str);
-    }
+    fn write_file(&mut self, file: &mut File, location: &str);
+}
+
+enum WaveType {
+    Sine,
+    Cosine,
+}
+
+/*
+    The idea at the time of writing this is the other parameters like the FileEncoding as defined above, will be stored in the specific object and can be referenced internally
+ */
+pub trait FileEncoding{
+    fn left_to_right(&self);
+    fn top_to_bottom(&self);
+    fn wave(&self, wave_type: WaveType, amplitude: f32, phase: f32, frequency: f32) -> Vec<P>;
+
+}
